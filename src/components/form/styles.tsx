3526e8a623ddcd85bb7787c31327868e1ccbb475
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-11-11 03:10:32 +0100

    style: create other file for style

---
+++ src/components/form/styles.tsx
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  input: {
+    margin: '1em',
+  },
+}));
