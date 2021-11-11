7eecb452defeaef82e20ed537e3bf920eda444dc
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-11-11 03:11:25 +0100

    refactor: other part

--- server/index.php
+++ server/index.php
@@ -19,11 +19,16 @@ $waittimeport = $_POST['waittimeport'];
 $waittimeunloader = $_POST['waittimeunloader'];
 $waittimesearchgoods = $_POST['waittimesearchgoods'];
 $waittimeemptygoods = $_POST['waittimeemptygoods'];
+$requireTime = $_POST['requiretime'];
+$mileage = $_POST['mileage'];
+$storeName = $_POST['storename'];
+$city = $_POST['city'];
+$quantity = $_POST['quantity'];
+$otherinfo = $_POST['otherinfo'];
 
 $query = "INSERT INTO avvikelse 
-(firstName,sureName,phoneNumber,driverNumber,loadNumber,date,time,waitTimeGuard,waitTimePort,waitTimeUnloader,waitTimeSearchGoods,waitTimeEmptyGoods) 
-VALUES('$firstname','$surename','$phonenumber','$drivernumber','$loadnumber','$date','$time','$waittimeguard','$waittimeport','$waittimeunloader','$waittimesearchgoods','$waittimeemptygoods')";
-
+(firstName,sureName,phoneNumber,driverNumber,loadNumber,date,time,waitTimeGuard,waitTimePort,waitTimeUnloader,waitTimeSearchGoods,waitTimeEmptyGoods,requireTime,mileage,storeName,city,quantity,otherInfo) 
+VALUES('$firstname','$surename','$phonenumber','$drivernumber','$loadnumber','$date','$time','$waittimeguard','$waittimeport','$waittimeunloader','$waittimesearchgoods','$waittimeemptygoods','$requireTime','$mileage','$storeName','$city','$quantity','$otherinfo')";
 
 if(mysqli_query($conn, $query)){
     echo "Data has been inserted successfully";
