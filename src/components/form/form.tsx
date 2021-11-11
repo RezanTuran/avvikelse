419951b89de992c8b4dbfe403560c5cb26dce91f
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-11-11 03:11:14 +0100

    refactor: other part

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -6,10 +6,12 @@ import {
   Typography,
   useMediaQuery,
 } from '@material-ui/core';
+import useStyles from './styles';
 import Axios from 'axios';
 
 const Form = () => {
   const isMobile = useMediaQuery('(min-width:767px)');
+  const classes = useStyles();
 
   const [firstName, setFirstName] = useState('');
   const [sureName, setSureName] = useState('');
@@ -23,11 +25,17 @@ const Form = () => {
   const [waitTimeUnloader, setWaitTimeUnloader] = useState('');
   const [waitTimeSearchGoods, setWaitTimeSearchGoods] = useState('');
   const [waitTimeEmptyGoods, setWaitTimeEmptyGoods] = useState('');
+  const [requireTime, setRequireTime] = useState('');
+  const [mileage, setMileage] = useState('');
+  const [storeName, setStoreName] = useState('');
+  const [city, setCity] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [otherInfo, setOtherInfo] = useState('');
 
   const sendRapport = (e: any) => {
     e.preventDefault();
-
     let formData = new FormData();
+
     formData.append('name', firstName);
     formData.append('surename', sureName);
     formData.append('phonenumber', phoneNumber);
@@ -40,6 +48,12 @@ const Form = () => {
     formData.append('waittimeunloader', waitTimeUnloader);
     formData.append('waittimesearchgoods', waitTimeSearchGoods);
     formData.append('waittimeemptygoods', waitTimeEmptyGoods);
+    formData.append('requiretime', requireTime);
+    formData.append('mileage', mileage);
+    formData.append('storename', storeName);
+    formData.append('city', city);
+    formData.append('quantity', quantity);
+    formData.append('otherinfo', otherInfo);
 
     const url = 'http://localhost:80/';
     Axios.post(url, formData)
@@ -48,20 +62,13 @@ const Form = () => {
   };
 
   return (
-    <Grid
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
+    <Grid className={classes.wrapper}>
       <Typography variant="h5" align="center" color="primary">
         ICA AVVIKELSERAPPORT
       </Typography>
       <Grid
+        className={classes.container}
         style={{
-          display: 'flex',
-          flexDirection: 'column',
           width: isMobile ? '50%' : '100%',
         }}
       >
@@ -69,7 +76,7 @@ const Form = () => {
           type="number"
           label="LASS Nummer"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setLoadNumber(e.target.value);
           }}
@@ -78,7 +85,7 @@ const Form = () => {
           type="date"
           variant="outlined"
           label={isMobile ? '' : 'Datum'}
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setDate(e.target.value);
           }}
@@ -87,7 +94,7 @@ const Form = () => {
           type="text"
           label="Namn Chaufför"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setFirstName(e.target.value);
           }}
@@ -96,7 +103,7 @@ const Form = () => {
           type="text"
           label="Efternamn Chaufför"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setSureName(e.target.value);
           }}
@@ -105,7 +112,7 @@ const Form = () => {
           type="number"
           label="Chaufförs nummer"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setDriverNumber(e.target.value);
           }}
@@ -114,7 +121,7 @@ const Form = () => {
           type="number"
           label="Mobilnummer"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setPhoneNumber(e.target.value);
           }}
@@ -127,7 +134,7 @@ const Form = () => {
           type="text"
           label="Invänta vakt för öppning av port (Minuter)"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setWaitTimeGuard(e.target.value);
           }}
@@ -136,7 +143,7 @@ const Form = () => {
           type="number"
           label="Väntan på hänvisad last port (Minuter)"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setWaitTimePort(e.target.value);
           }}
@@ -145,7 +152,7 @@ const Form = () => {
           type="number"
           label="Väntan på utlastare (Minuter)"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setWaitTimeUnloader(e.target.value);
           }}
@@ -154,7 +161,7 @@ const Form = () => {
           type="number"
           label="Uppsökning av ej funnet gods (Minuter)"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setWaitTimeSearchGoods(e.target.value);
           }}
@@ -163,17 +170,80 @@ const Form = () => {
           type="number"
           label="Väntan för avlastning av tomgods (Minuter)"
           variant="outlined"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onChange={(e) => {
             setWaitTimeEmptyGoods(e.target.value);
           }}
         />
 
+        <Typography variant="h6" align="center" color="primary">
+          UPPGIFTER VID ÖVEDRAG OCH STÄLLA SLÄP
+        </Typography>
+        <TextField
+          type="number"
+          label="Tidsåtgång (Minuter)"
+          variant="outlined"
+          className={classes.input}
+          onChange={(e) => {
+            setRequireTime(e.target.value);
+          }}
+        />
+        <TextField
+          type="number"
+          label="Extra körsträcka (KM)"
+          variant="outlined"
+          className={classes.input}
+          onChange={(e) => {
+            setMileage(e.target.value);
+          }}
+        />
+        <TextField
+          type="text"
+          label="Butiksnamn"
+          variant="outlined"
+          className={classes.input}
+          onChange={(e) => {
+            setStoreName(e.target.value);
+          }}
+        />
+        <TextField
+          type="text"
+          label="Stad"
+          variant="outlined"
+          className={classes.input}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <TextField
+          type="text"
+          label="Antal ib (Överdrag)"
+          variant="outlined"
+          className={classes.input}
+          onChange={(e) => {
+            setQuantity(e.target.value);
+          }}
+        />
+        <Typography variant="h6" align="center" color="primary">
+          ÖVRIGA AVVIKELSER: SPECIFICERA NOGGRANT
+        </Typography>
+        <Typography variant="body1" align="center" color="secondary">
+          Exempel: väntan för avlastning i butik, försening på grund av
+          omständigheter.
+        </Typography>
+        <TextField
+          type="text"
+          variant="outlined"
+          className={classes.input}
+          onChange={(e) => {
+            setOtherInfo(e.target.value);
+          }}
+        />
         <Button
           variant="contained"
           color="primary"
           type="submit"
-          style={{ margin: '1em' }}
+          className={classes.input}
           onClick={sendRapport}
         >
           Skicka
