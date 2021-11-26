39df1dbbe7d3b9cae8ad224ee325d959bdf5ed3c
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-11-26 02:54:22 +0100

    refactor: page title fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Grid } from '@material-ui/core';
 import Home from './pages/home';
 import Router from './router';
+
 function App() {
   return (
     <Grid className="App">
--- src/components/helsingborgForm/helsingborgForm.tsx
+++ src/components/helsingborgForm/helsingborgForm.tsx
@@ -13,6 +13,7 @@ import useStyles from './styles';
 import Axios from 'axios';
 import Rapports from '../rapports/rapports';
 import emailjs from 'emailjs-com';
+import { Helmet } from 'react-helmet';
 
 const Form = () => {
   const isMobile = useMediaQuery('(min-width:767px)');
@@ -74,10 +75,10 @@ const Form = () => {
     sendRapport(e);
     emailjs
       .sendForm(
-        'service_9eavzwt',
-        'template_ud9xd4c',
+        'service_b33p05r',
+        'template_m1ypj2a',
         e.target,
-        'user_5dRo73CQVOaKHBKHe6xgQ'
+        'user_NqjwPyD6lDtwweTdX8Bmu'
       )
       .then(
         (result) => {
@@ -91,204 +92,210 @@ const Form = () => {
   }
 
   return (
-    <form onSubmit={sendEmail}>
-      <Grid className={classes.wrapper}>
-        <Typography variant="h5" align="center" color="primary">
-          ICA AVVIKELSERAPPORT HELSINGBORG
-        </Typography>
-        <Grid
-          className={classes.container}
-          style={{
-            width: isMobile ? '50%' : '100%',
-          }}
-        >
-          <TextField
-            type="number"
-            name="lass_number"
-            label="LASS Nummer"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setLoadNumber(e.target.value);
-            }}
-          />
-          <TextField
-            type="date"
-            name="date"
-            variant="outlined"
-            label={isMobile ? '' : 'Datum'}
-            className={classes.input}
-            onChange={(e) => {
-              setDate(e.target.value);
-            }}
-          />
-          <TextField
-            type="text"
-            label="Namn Chaufför"
-            name="driver_name"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
-          />
-          <TextField
-            type="text"
-            label="Efternamn Chaufför"
-            name="driver_surename"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setSureName(e.target.value);
-            }}
-          />
-          <TextField
-            type="number"
-            label="Chaufförs nummer"
-            name="driver_number"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setDriverNumber(e.target.value);
-            }}
-          />
-          <TextField
-            type="number"
-            label="Mobilnummer"
-            name="phone_number"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setPhoneNumber(e.target.value);
-            }}
-          />
-          <Grid className={classes.checkbox}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  onChange={(e) => {
-                    setPickup(e.target.checked);
-                  }}
-                  style={{ color: '#00A093' }}
-                />
-              }
-              label="Hämtning Helsingborg"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  onChange={(e) => {
-                    setDistribution(e.target.checked);
-                  }}
-                  style={{ color: '#00A093' }}
-                />
-              }
-              label="Ditrubition"
-            />
-          </Grid>
-          <Typography variant="h6" align="center" color="primary">
-            AVVIKELSER LASTNING ICA HELSINGBORG (FRYS)
-          </Typography>
+    <Grid>
+      <Helmet>
+        <title>ICA Helsingborg Frys</title>
+      </Helmet>
 
-          <TextField
-            type="text"
-            name="wait_time_guard"
-            label="Invänta vakt för öppning av port (Minuter)"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setWaitTimeGuard(e.target.value);
-            }}
-          />
-          <TextField
-            type="number"
-            name="wait_time_port"
-            label="Väntan på hänvisad last port (Minuter)"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setWaitTimePort(e.target.value);
-            }}
-          />
-          <TextField
-            type="number"
-            label="Väntan på utlastare (Minuter)"
-            name="wait_time_loader"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setWaitTimeUnloader(e.target.value);
-            }}
-          />
-          <TextField
-            type="number"
-            label="Uppsökning av ej funnet gods (Minuter)"
-            name="wait_time_loader"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setWaitTimeSearchGoods(e.target.value);
-            }}
-          />
-          <TextField
-            type="number"
-            label="Vänta på port OMEX Exportgatan (Minuter)"
-            name="search_load"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setWaitTimeOmexPort(e.target.value);
-            }}
-          />
-          <TextField
-            type="number"
-            label="Medtaget gods (Antal)"
-            name="wait_time_empty_load"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setIncludedLoad(e.target.value);
-            }}
-          />
-
-          <TextField
-            type="number"
-            label="Lass ej färdigtplockat (Minuter)"
-            name="minut"
-            variant="outlined"
-            className={classes.input}
-            onChange={(e) => {
-              setLoadNotRady(e.target.value);
-            }}
-          />
-
-          <Typography variant="h6" align="center" color="primary">
-            ÖVRIGA AVVIKELSER: SPECIFICERA NOGGRANT
+      <form onSubmit={sendEmail}>
+        <Grid className={classes.wrapper}>
+          <Typography variant="h5" align="center" color="primary">
+            ICA AVVIKELSERAPPORT HELSINGBORG
           </Typography>
-          <Typography variant="body1" align="center" color="secondary">
-            Exempel: Väntan för avlastning i butik, försening på grund av
-            omständigheter.
-          </Typography>
-          <TextareaAutosize
-            rows={10}
-            name="other_info"
-            className={classes.input}
-            onChange={(e) => {
-              setOtherInfo(e.target.value);
+          <Grid
+            className={classes.container}
+            style={{
+              width: isMobile ? '50%' : '100%',
             }}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            className={classes.input}
           >
-            Skicka
-          </Button>
+            <TextField
+              type="number"
+              name="lass_number"
+              label="LASS Nummer"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setLoadNumber(e.target.value);
+              }}
+            />
+            <TextField
+              type="date"
+              name="date"
+              variant="outlined"
+              label={isMobile ? '' : 'Datum'}
+              className={classes.input}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            />
+            <TextField
+              type="text"
+              label="Namn Chaufför"
+              name="driver_name"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+            <TextField
+              type="text"
+              label="Efternamn Chaufför"
+              name="driver_surename"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setSureName(e.target.value);
+              }}
+            />
+            <TextField
+              type="number"
+              label="Chaufförs nummer"
+              name="driver_number"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setDriverNumber(e.target.value);
+              }}
+            />
+            <TextField
+              type="number"
+              label="Mobilnummer"
+              name="phone_number"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+            />
+            <Grid className={classes.checkbox}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={(e) => {
+                      setPickup(e.target.checked);
+                    }}
+                    style={{ color: '#00A093' }}
+                  />
+                }
+                label="Hämtning Helsingborg"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={(e) => {
+                      setDistribution(e.target.checked);
+                    }}
+                    style={{ color: '#00A093' }}
+                  />
+                }
+                label="Ditrubition"
+              />
+            </Grid>
+            <Typography variant="h6" align="center" color="primary">
+              AVVIKELSER LASTNING ICA HELSINGBORG (FRYS)
+            </Typography>
+
+            <TextField
+              type="text"
+              name="wait_time_guard"
+              label="Invänta vakt för öppning av port (Minuter)"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setWaitTimeGuard(e.target.value);
+              }}
+            />
+            <TextField
+              type="number"
+              name="wait_time_port"
+              label="Väntan på hänvisad last port (Minuter)"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setWaitTimePort(e.target.value);
+              }}
+            />
+            <TextField
+              type="number"
+              label="Väntan på utlastare (Minuter)"
+              name="wait_time_loader"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setWaitTimeUnloader(e.target.value);
+              }}
+            />
+            <TextField
+              type="number"
+              label="Uppsökning av ej funnet gods (Minuter)"
+              name="wait_time_loader"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setWaitTimeSearchGoods(e.target.value);
+              }}
+            />
+            <TextField
+              type="number"
+              label="Vänta på port OMEX Exportgatan (Minuter)"
+              name="search_load"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setWaitTimeOmexPort(e.target.value);
+              }}
+            />
+            <TextField
+              type="number"
+              label="Medtaget gods (Antal)"
+              name="wait_time_empty_load"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setIncludedLoad(e.target.value);
+              }}
+            />
+
+            <TextField
+              type="number"
+              label="Lass ej färdigtplockat (Minuter)"
+              name="minut"
+              variant="outlined"
+              className={classes.input}
+              onChange={(e) => {
+                setLoadNotRady(e.target.value);
+              }}
+            />
+
+            <Typography variant="h6" align="center" color="primary">
+              ÖVRIGA AVVIKELSER: SPECIFICERA NOGGRANT
+            </Typography>
+            <Typography variant="body1" align="center" color="secondary">
+              Exempel: Väntan för avlastning i butik, försening på grund av
+              omständigheter.
+            </Typography>
+            <TextareaAutosize
+              rows={10}
+              name="other_info"
+              className={classes.input}
+              onChange={(e) => {
+                setOtherInfo(e.target.value);
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              className={classes.input}
+            >
+              Skicka
+            </Button>
+          </Grid>
+          <Rapports />
         </Grid>
-        <Rapports />
-      </Grid>
-    </form>
+      </form>
+    </Grid>
   );
 };
 
