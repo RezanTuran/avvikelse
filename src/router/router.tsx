f10a1675f4791a43f7d361471c90c59c585dd547
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-12-03 03:31:38 +0100

    refactor: change to arrow function and  new comp

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import KungalvForm from '../components/kungalvForm';
 import HelsingborgForm from '../components/helsingborgForm';
+import Login from '../components/login';
+import Rapports from '../components/rapports';
 
-export default function App() {
+const App = () => {
   return (
     <Routes>
       <Route path="/kungalvform" element={<KungalvForm />} />
       <Route path="/helsingborgform" element={<HelsingborgForm />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/rapports" element={<Rapports />} />
     </Routes>
   );
-}
+};
+export default App;
