2f2dea0d1ce1db2d3adb159ce82a1b5625726b29
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2023-05-20 21:01:12 +0200

    refactor: new db name

--- server/db.php
+++ server/db.php
@@ -1,9 +1,10 @@
 <?php
-header("Access-Control-Allow-Origin: *"); //add this CORS header to enable any domain to send HTTP requests to these endpoints:
+header("Access-Control-Allow-Origin: *");
+
 $host = "localhost"; 
 $user = "root"; 
 $password = "root"; 
-$dbname = "react_php"; 
+$dbname = "icaavvikelse"; 
  
 $conn = mysqli_connect($host, $user, $password,$dbname);
 
@@ -11,3 +12,8 @@ $method = $_SERVER['REQUEST_METHOD'];
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
+
+
+
+?>
