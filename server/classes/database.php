0bb5341e26d004238e97bf01ab6c82c1c3fccf06
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2023-05-23 22:55:25 +0200

    bug: bug is fixed, could not get data as json

--- server/classes/database.php
+++ server/classes/database.php
@@ -8,13 +8,11 @@ class Database {
         $user = 'root';
         $password = 'root';
 
-    try {
+ 
     $this->connection = new PDO($dsn, $user, $password);
     $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    }
+
+   
 
 }
 }
