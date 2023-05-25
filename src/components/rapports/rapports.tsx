8ce6c2305c662677c0a62a39bf9adee6229e087f
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2023-05-25 06:42:30 +0200

    refactor: get endpoint is ready

--- src/components/rapports/rapports.tsx
+++ src/components/rapports/rapports.tsx
@@ -5,9 +5,7 @@ const Rapports = () => {
   const [rapports, setRapports] = useState([]);
 
   useEffect(() => {
-    Axios.get(
-      'http://localhost:8888/recievers/truckRecievers.php?action=getTruckRepairs'
-    )
+    Axios.get(`${process.env.REACT_APP_GET || ''}?action=getTruckRepairs`)
       .then((response: any) => {
         setRapports(response.data);
 
@@ -19,15 +17,20 @@ const Rapports = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       {rapports.map((rapport: any) => (
-        <div>
-          <p>{rapport.regNr}</p>
+        <div
+          style={{ border: '4px solid black', margin: '5px', padding: '15px' }}
+        >
           <img
             src={require(`../../assets/img/${rapport.image}`).default}
             alt="React Logo"
             style={{ height: 250, width: 200 }}
           />
+          <p>{rapport.regNr}</p>
+          <p>{rapport.repairArea}</p>
+          <p>{rapport.explainRepair}</p>
+          <p>{rapport.datum}</p>
         </div>
       ))}
     </div>
